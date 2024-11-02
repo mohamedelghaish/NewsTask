3a29082532da11e26def9edc19c18a13cda10a2d
mohamedelghaish <149378056+mohamedelghaish@users.noreply.github.com>
2024-11-02 17:56:12 +0200

    implement home view model

---
+++ NewsTask/Modules/Home/View/ArticleCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ArticleCollectionViewCell.swift
+//  NewsTask
+//
+//  Created by Mohamed Kotb Saied Kotb on 02/11/2024.
+//
+
+import UIKit
+
+class ArticleCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ NewsTask/Modules/Home/ViewModel/HomeViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  HomeViewModel.swift
+//  NewsTask
+//
+//  Created by Mohamed Kotb Saied Kotb on 02/11/2024.
+//
+
+import Foundation
+import Combine
+
+class HomeViewModel: ObservableObject {
+    @Published var articles: [Article] = []
+    private var cancellables = Set<AnyCancellable>()
+    
+    func fetchArticles(query: String, fromDate: Date?) {
+        NetworkManager.shared.fetchArticles(query: query, fromDate: fromDate)
+            .sink(receiveCompletion: { completion in
+                if case .failure(let error) = completion {
+                    print("Failed to fetch articles:", error)
+                }
+            }, receiveValue: { [weak self] articles in
+                self?.articles = articles
+            })
+            .store(in: &cancellables)
+    }
+}
