d454fcf69c12260d07fbaa06e4c0b75624490d6d
mohamedelghaish <149378056+mohamedelghaish@users.noreply.github.com>
2024-11-02 17:24:48 +0200

    Initial Commit

---
+++ NewsTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsTask
+//
+//  Created by Mohamed Kotb Saied Kotb on 02/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
